393233a81a4948361b57d5dc4bdbb0e4604da2e6
Omji_Kesharwani <kesharwaniomji000@gmail.com>
2025-08-14 22:48:27 +0530

    Change

--- project-1/src/tests/sum.test.ts
+++ project-1/src/tests/sum.test.ts
@@ -12,3 +12,14 @@ describe("POST /sum", () => {
         expect(body).toEqual({ answer: 3, id: expect.any(Number) });
     });
 })
+
+describe("POST /sum", () => {
+    it("should sum add 2 negative numbers", async () => {
+        const { status, body } = await request(app).post('/sum').send({
+            a: -1,
+            b: -2
+        })
+        expect(status).toBe(200);
+        expect(body).toEqual({ answer: -3, id: expect.any(Number) });
+    });
+})
